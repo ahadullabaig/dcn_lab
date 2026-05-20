a5d2546b204e5855bcac5737ef0937f4d9e85e06
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-21 02:10:02 +0530

    modified character_stuffing.cpp

--- character_stuffing.cpp
+++ character_stuffing.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-// Character stuffing:
-// Frame is delimited by DLE STX ... DLE ETX
-// Any DLE inside the payload is escaped by inserting another DLE.
-// Here we use: DLE='X', STX='S', ETX='E' for simplicity.
-
-int main() {
+int main()
+{
     string data;
+
     cout << "Enter data (no spaces): ";
     cin >> data;
 
-    // ---- Stuffing (sender side) ----
-    string frame = "XS"; // DLE STX
-    for (char c : data) {
-        if (c == 'X') frame += "XX"; // escape DLE
+    string frame = "XS";
+
+    for (char c : data)
+    {
+        if (c == 'X') frame += "XX";
         else frame += c;
     }
-    frame += "XE"; // DLE ETX
+
+    frame += "XE";
 
     cout << "Stuffed frame: " << frame << endl;
 
-    // ---- Destuffing (receiver side) ----
     string original = "";
-    int i = 2; // skip starting DLE STX
-    while (i < (int)frame.size() - 2) { // stop before DLE ETX
-        if (frame[i] == 'X' && frame[i+1] == 'X') {
+
+    int i = 2;
+
+    while (i < (int)frame.size() - 2)
+    {
+        if (frame[i] == 'X' && frame[i+1] == 'X')
+        {
             original += 'X';
             i += 2;
-        } else {
+        }
+        else
+        {
             original += frame[i];
             i++;
         }
     }
+    
     cout << "Destuffed data: " << original << endl;
 
     return 0;
