b24b1a3a144c2e15425f5331fc9312139d42be3c
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-21 02:12:11 +0530

    modified bit_stuffing.cpp

--- bit_stuffing.cpp
+++ bit_stuffing.cpp
@@ -1,45 +1,59 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-// Bit stuffing:
-// Whenever sender sees 5 consecutive 1's in the data,
-// it inserts a 0 right after them so the bit pattern
-// can never be confused with the frame delimiter 01111110.
-
-int main() {
+int main()
+{
     string bits;
+
     cout << "Enter bit string (0s and 1s): ";
     cin >> bits;
 
-    // ---- Stuffing ----
     string stuffed = "";
+
     int count = 0;
-    for (char b : bits) {
+
+    for (char b : bits)
+    {
         stuffed += b;
-        if (b == '1') {
+    
+        if (b == '1')
+        {
             count++;
-            if (count == 5) {
-                stuffed += '0'; // stuff a 0
+        
+            if (count == 5)
+            {
+                stuffed += '0';
                 count = 0;
             }
-        } else count = 0;
+        }
+        else count = 0;
     }
+
     cout << "Stuffed bits:   " << stuffed << endl;
 
-    // ---- Destuffing ----
     string destuffed = "";
+
     count = 0;
-    for (int i = 0; i < (int)stuffed.size(); i++) {
+
+    for (int i = 0; i < (int)stuffed.size(); i++)
+    {
         destuffed += stuffed[i];
-        if (stuffed[i] == '1') {
+    
+        if (stuffed[i] == '1')
+        {
             count++;
-            if (count == 5) {
-                i++; // skip the stuffed 0
+        
+            if (count == 5)
+            {
+                i++;
                 count = 0;
             }
-        } else count = 0;
+        }
+        else count = 0;
     }
+    
     cout << "Destuffed bits: " << destuffed << endl;
 
     return 0;
