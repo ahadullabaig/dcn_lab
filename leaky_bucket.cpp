c45add7d112e4917f9b2485cd6f85bff29307df4
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-20 19:03:29 +0530

    modified leaky_bucket.cpp

--- leaky_bucket.cpp
+++ leaky_bucket.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
     int bucketSize, outputRate, n;
 
     cout << "Enter bucket size: ";
     cin >> bucketSize;
-    cout << "Enter output (leak) rate: ";
+
+    cout << "Enter output rate: ";
     cin >> outputRate;
-    cout << "Enter number of seconds (packets arriving): ";
+    
+    cout << "Enter number of seconds: ";
     cin >> n;
 
-    int bucket = 0;  // current water (data) in bucket
+    int bucket = 0;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         int incoming;
+
         cout << "\nSecond " << i << " - Enter incoming packet size: ";
         cin >> incoming;
 
-        // Step 1: try to add incoming packet
-        if (bucket + incoming > bucketSize) {
+        if (bucket + incoming > bucketSize)
+        {
             int dropped = (bucket + incoming) - bucketSize;
+            
             cout << "Bucket overflow! Dropped " << dropped << " units.\n";
+            
             bucket = bucketSize;
-        } else {
+        }
+        else
+        {
             bucket += incoming;
+            
             cout << "Added " << incoming << " units to bucket.\n";
         }
 
-        // Step 2: leak out at fixed rate
         int sent = (bucket >= outputRate) ? outputRate : bucket;
+
         bucket -= sent;
-        cout << "Transmitted " << sent << " units. Bucket now holds "
-             << bucket << "/" << bucketSize << " units.\n";
+        
+        cout << "Transmitted " << sent << " units.";
+        cout << "\nBucket now holds " << bucket << "/" << bucketSize << " units.\n";
     }
 
     return 0;
